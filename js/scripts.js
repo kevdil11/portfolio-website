897a29bb3ca234a4ec9e36d06c8f4269aae4be2c
kevdil11 <100292333+kevdil11@users.noreply.github.com>
2022-04-18 21:15:31 -0600

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 console.log("Hello world")
 
-
-function menuToggle() {
+function menuToggle () {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
     x.className += ' responsive';
